cc4a95e2203fbd7140445147b88177bfbf72be47
Kevin Pheasey <kevin.pheasey@gmail.com>
2014-06-03 14:34:12 -0400

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "ln -nfs #{config.shared_path}/config/local.xml #{config.release_path}/app/etc/local.xml"
+run "ln -nfs #{config.shared_path}/config/local.xml #{config.release_path}/public/app/etc/local.xml"
